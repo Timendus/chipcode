f0e69375d3972192ba3edc45fde00ec4227a6580
Timendus <mail@timendus.com>
2022-11-15 22:09:17 +0100

    Improve Octopus a bit, update preprocessor syntas

--- octopus.js
+++ octopus.js
@@ -6,9 +6,10 @@
  * with any text file.
  *
  * Features:
- *    * Include other files (`#include <filename>`)
- *    * Make code inclusion decisions (`#if <option>` / `#else` / `#end`)
- *    * Mark blocks as code or data, and get them re-ordered (`#code` / `#data`)
+ *    * Include other files (`:include <filename>`)
+ *    * Make code inclusion decisions based on options (`:if <option>` / `:unless <option>` / `:else` / `:end`)
+ *    * Set and reset options (`:const SOME_OPTION 1` and `:const SOME_OPTION 0`)
+ *    * Mark blocks as code or data, and get them re-ordered (`:code` / `:data`)
  *
  * Usage:
  *     ./octopus.js <input file> <ouput file> <option 1> <option 2> ...
@@ -19,87 +20,144 @@ const path    = require('path');
 const input   = process.argv[2];
 const output  = process.argv[3];
 const options = process.argv.slice(4, process.argv.length);
-const dirname = path.dirname(input);
 
-const result = reorder(octopussify(loadFile(input), dirname));
+// Check input
+if ( !input || !output ) {
+  console.error("Input and output file are required parameters!\n\nUsage:\n   octopus <input file> <ouput file> <option 1> <option 2> ...");
+  process.exit();
+}
+
+// Define some regular expressions to make our matchers easier to read
+const capture = '([\\w\\-]+)';
+const whitespace = '\\s+';
+
+// Kick off the Octopussification!
+const result = reorder(octopussify(loadFile(input), path.dirname(input)));
 fs.writeFileSync(output, result);
 
-function loadFile(filename) {
-  return fs.readFileSync(filename).toString();
-}
+
+// Main function doing the heavy lifting
 
 function octopussify(file, filepath) {
-  let outputting = [ true ];
-  return file.split('\n')
-             .filter(line => conditionals(line, outputting))
-             .map(line => includes(line, filepath))
-             .join('\n')
-             + "#code\n";
+  const outputting = [ true ];
+  const mode = [ 'code' ];
+  return ":segment code\n" +
+    file.split('\n')
+        .filter((line, i) => conditionals(line, outputting, i + 1))
+        .map(line => includes(line, mode, filepath))
+        .join('\n');
 }
 
-function conditionals(line, outputting) {
+// Conditional code inclusion based on options, with :const, :if, :else et cetera
+
+function conditionals(line, outputting, lineNr) {
   let matches;
-  if ( matches = line.match(/^\s*#if (.*)\s*$/i) ) {
+
+  // Parse conditional inclusion of code
+  if ( matches = match(line, `:if${whitespace}${capture}`) ) {
     outputting.push(options.includes(matches[1]));
     return false;
   }
-  if ( line.match(/^\s*#else\s*$/i) ) {
+  if ( matches = match(line, `:unless${whitespace}${capture}`) ) {
+    outputting.push(!options.includes(matches[1]));
+    return false;
+  }
+  if ( match(line, ':else') ) {
     outputting[outputting.length - 1] = !outputting[outputting.length - 1];
     return false;
   }
-  if ( line.match(/^\s*#end\s*$/i) ) {
+  if ( match(line, ':end') ) {
     outputting.pop();
     return false;
   }
-  return outputting.every(o => o);
+
+  // Should we output / interpret the current line..?
+  if ( !outputting.every(o => o) )
+    return false;
+
+  // Parse :const
+  // Values 0 and "false" are "disable option"
+  // Other values are "enable option"
+  if ( (matches = match(line, `:const${whitespace}${capture}${whitespace}${capture}`)) ) {
+    if ( matches[2] == '0' || matches[2].toLowerCase() == 'false' ) {
+      const index = options.indexOf(matches[1]);
+      if ( index > -1 )
+        options.splice(index, 1);
+    } else {
+      if ( !options.includes(matches[1]) )
+        options.push(matches[1]);
+    }
+    return true;
+  }
+
+  // For debugging issues with your options
+  if ( match(line, ':dump-options') ) {
+    console.log(`Options on line ${lineNr}:`, options);
+    return false;
+  }
+
+  return true;
 }
 
-function includes(line, filepath) {
+// Including other source files with :include
+
+function includes(line, mode, filepath) {
+  if ( match(line, ':code') ) mode[0] = 'code';
+  if ( match(line, ':data') ) mode[0] = 'data';
+  
   let matches;
-  if ( matches = line.match(/^\s*#include "(.*)"\s*$/i) ) {
+  if ( matches = match(line, `:include${whitespace}["'](.*)["']`) ) {
     const fileToInclude = filepath + path.sep + matches[1];
-    return octopussify(loadFile(fileToInclude), path.dirname(fileToInclude));
+    return octopussify(loadFile(fileToInclude), path.dirname(fileToInclude)) +
+      (mode[0] == 'code' ? '\n:segment code' : '\n:segment data');
   } else {
     return line;
   }
 }
 
+// Reordering :code and :data
+
 function reorder(file) {
   const lines = file.split('\n');
-  let selected = true;
-  const code = lines.filter(line => {
-    if ( line.match(/^\s*#code\s*$/i) ) {
-      selected = true;
-      return false;
-    }
-    if ( line.match(/^\s*#data\s*$/i) ) {
-      selected = false;
-      return false;
-    }
-    return selected;
-  });
-  selected = false;
-  const data = lines.filter(line => {
-    if ( line.match(/^\s*#data\s*$/i) ) {
-      selected = true;
-      return false;
-    }
-    if ( line.match(/^\s*#code\s*$/i) ) {
-      selected = false;
-      return false;
-    }
-    return selected;
-  });
+  const code = lines.filter(selectLines(true));
+  const data = lines.filter(selectLines(false));
   reportSize(code, data);
   return code.join('\n') + data.join('\n');
 }
+
+function selectLines(onlyCode = true) {
+  let selected = onlyCode;
+  return line => {
+    if ( match(line, `:segment${whitespace}code`) ) {
+      selected = onlyCode;
+      return false;
+    }
+    if ( match(line, `:segment${whitespace}data`) ) {
+      selected = !onlyCode;
+      return false;
+    }
+    return selected;
+  };
+}
+
+// Helpers
+
+function loadFile(filename) {
+  return fs.readFileSync(filename).toString();
+}
+
+function match(line, command) {
+  return line.match(new RegExp(`^\\s*${command}\\s*(#.*)?$`, 'i'));
+}
+
+// Calculating and reporting # lines and # bytes
 
 function reportSize(code, data) {
   const codeSize = calcSize(code);
   const dataSize = calcSize(data);
   const totalSize = codeSize + dataSize;
-  console.log(`${code.length} lines (roughly ${codeSize} bytes) of code`);
-  console.log(`${data.length} lines (roughly ${dataSize} bytes) of data`);
+  console.log(`${code.length} lines (roughly ${codeSize} bytes) marked as code`);
+  console.log(`${data.length} lines (roughly ${dataSize} bytes) marked as data`);
   if ( totalSize > 3216  ) console.warn("! Be warned: estimated program size exceeds VIP memory size");
   if ( totalSize > 3583  ) console.warn("! Be warned: estimated program size exceeds SCHIP memory size");
   if (  codeSize > 3583  ) console.warn("! Be warned: estimated code size exceeds XO-CHIP executable memory size");
@@ -109,7 +167,7 @@ function reportSize(code, data) {
 function calcSize(lines) {
   lines = lines.filter(l => l)                     // Skip empty lines
                .filter(l => !l.match(/^\s*#.*$/))  // Skip comments
-               .filter(l => !l.match(/^\s*:.*$/))  // Skip labels
+               .filter(l => !l.match(/^\s*:.*$/))  // Skip labels and meta-instructions
 
   const numbers = /^(?:\s+[0-9a-fx]+)+\s*$/i;
   const number = /[0-9a-fx]+/gi;
