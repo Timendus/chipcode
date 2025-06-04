0329269cbecd1bccb61e18c69248e7cd78dadddc
Timendus <mail@timendus.com>
2025-06-04 14:50:36 +0200

    Add tests for the assembler

--- octopus2/tests/regression-tests.go
+++ octopus2/tests/regression-tests.go
@@ -20,12 +20,43 @@ func main() {
 	for _, file := range dir {
 		if file.IsDir() {
 			inputFile := testsDirectory + file.Name() + "/src/index.8o"
+
+			/* Test Octopussification */
+
 			outputFile := testsDirectory + file.Name() + "/output.8o"
 			expectedFile := testsDirectory + file.Name() + "/expected.8o"
 
-			if !runCommand(octopus+" "+inputFile+" "+outputFile) ||
-				!runCommand("git diff --no-index --color "+expectedFile+" "+outputFile) {
+			if !runCommand(octopus + " " + inputFile + " " + outputFile) {
+				fmt.Println("Could not Octopussify " + inputFile)
 				allGood = false
+				continue
+			}
+
+			if !runCommand("git diff --no-index --color " + expectedFile + " " + outputFile) {
+				fmt.Println("No match for output " + expectedFile + " and " + outputFile)
+				allGood = false
+				continue
+			}
+
+			/* Test Octopussification + assembly */
+
+			outputBinary := testsDirectory + file.Name() + "/output.ch8"
+			expectedBinary := testsDirectory + file.Name() + "/expected.ch8"
+			outputHex := testsDirectory + file.Name() + "/output.hex"
+			expectedHex := testsDirectory + file.Name() + "/expected.hex"
+
+			if !runCommand(octopus + " " + inputFile + " " + outputBinary) {
+				fmt.Println("Could not assemble " + inputFile)
+				allGood = false
+				continue
+			}
+
+			hexDump(expectedBinary, expectedHex)
+			hexDump(outputBinary, outputHex)
+			if !runCommand("git diff --no-index --color " + expectedHex + " " + outputHex) {
+				fmt.Println("No match for binaries " + expectedBinary + " and " + outputBinary)
+				allGood = false
+				continue
 			}
 		}
 	}
@@ -48,4 +79,15 @@ func runCommand(command string) bool {
 		return false
 	}
 	return true
+}
+
+func hexDump(inputFile string, outputFile string) error {
+	cmd := exec.Command("hexdump", "-v", "-C", inputFile)
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println(string(output))
+		return err
+	}
+	os.WriteFile(outputFile, output, 0644)
+	return nil
 }
