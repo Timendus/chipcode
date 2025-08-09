9241414cc264af920a6d982465bd98b3d1f411f7
Timendus <mail@timendus.com>
2025-08-09 12:42:50 +0200

    Don't use ANSI colors or UTF8 on Windows. Added flags to force it one way or another, sent output to stderr instead. Pretty big change to the API.

--- octopus2/src/main.go
+++ octopus2/src/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path"
+	"runtime"
 	"strings"
 	"time"
 
@@ -11,56 +13,74 @@ import (
 	"github.com/timendus/chipcode/octopus2/src/preprocessor"
 )
 
+var (
+	USE_COLOR      = flag.Bool("color", false, "Use ANSI codes for color output to the terminal")
+	DONT_USE_COLOR = flag.Bool("no-color", false, "Do not use ANSI codes for color output to the terminal")
+	INPUT_FILE     = flag.String("input", "", "The path of the input file")
+	OUTPUT_FILE    = flag.String("output", "STDOUT", "The path of the output file")
+	USING_COLOR    = runtime.GOOS != "windows" // Disable ANSI colors by default on Windows
+)
+
 func main() {
 	startTime := time.Now()
 
 	// Parse parameters
-	if len(os.Args) < 3 {
-		fmt.Println("\033[91;1mInput and output file are required parameters\033[0m\nUsage:\n   octopus <input file> <ouput file> <option 1> <option 2> ...")
+	flag.StringVar(INPUT_FILE, "i", *INPUT_FILE, "Alias for -input")
+	flag.StringVar(OUTPUT_FILE, "o", *OUTPUT_FILE, "Alias for -output")
+	flag.Usage = print_usage
+	flag.Parse()
+	USING_COLOR = (USING_COLOR || *USE_COLOR) && !*DONT_USE_COLOR
+
+	if *INPUT_FILE == "" {
+		fmt.Fprintln(os.Stderr, bad("Input file is a required parameter\n"))
+		print_usage()
 		os.Exit(1)
 	}
-	input := os.Args[1]
-	output := os.Args[2]
+
 	options := map[string]bool{}
-	for _, option := range os.Args[3:] {
+	for _, option := range flag.Args() {
 		options[option] = true
 	}
 
-	if strings.ToLower(path.Ext(input)) != ".8o" {
-		fmt.Printf("\033[91;1mDon't know how to convert '%s' to '%s'\033[0m\n", path.Ext(input), path.Ext(output))
+	if strings.ToLower(path.Ext(*INPUT_FILE)) != ".8o" {
+		fmt.Fprintf(os.Stderr, bad("Don't know how to handle '%s' file\n"), path.Ext(*INPUT_FILE))
 		os.Exit(1)
 	}
 
-	switch strings.ToLower(path.Ext(output)) {
-	case ".8o":
-		fmt.Printf("Octopussifying '%s' 🡆 '%s'\n", input, output)
+	switch strings.ToLower(path.Ext(*OUTPUT_FILE)) {
+	case ".8o", "":
+		fmt.Fprintf(os.Stderr, "Octopussifying '%s' --> '%s'\n", *INPUT_FILE, *OUTPUT_FILE)
 
 		// Octopussify the input file
-		octopussified, errs := preprocessor.Octopussify(input, options)
+		octopussified, errs := preprocessor.Octopussify(*INPUT_FILE, options)
 		if len(errs) > 0 {
-			fmt.Println("\033[91;1mCould not complete octopussification due to the following errors:\033[0m")
+			fmt.Fprintln(os.Stderr, bad("Could not complete octopussification due to the following errors:"))
 			for _, error := range errs {
-				fmt.Println("   - ", error)
+				fmt.Fprintln(os.Stderr, "   - ", error)
 			}
 			os.Exit(1)
 		}
 
 		// And output to the destination file
-		err := os.WriteFile(output, []byte(octopussified), 0644)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+		if *OUTPUT_FILE == "STDOUT" {
+			fmt.Print(octopussified)
+		} else {
+			err := os.WriteFile(*OUTPUT_FILE, []byte(octopussified), 0644)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
 		}
 
 	case ".ch8":
-		fmt.Printf("Octopussifying and assembling '%s' 🡆 '%s'\n", input, output)
+		fmt.Fprintf(os.Stderr, "Octopussifying and assembling '%s' --> '%s'\n", *INPUT_FILE, *OUTPUT_FILE)
 
 		// Octopussify the input file
-		octopussified, errs := preprocessor.Octopussify(input, options)
+		octopussified, errs := preprocessor.Octopussify(*INPUT_FILE, options)
 		if len(errs) > 0 {
-			fmt.Println("\033[91;1mCould not complete octopussification due to the following errors:\033[0m")
+			fmt.Fprintln(os.Stderr, bad("Could not complete octopussification due to the following errors:"))
 			for _, error := range errs {
-				fmt.Println("   - ", error)
+				fmt.Fprintln(os.Stderr, "   - ", error)
 			}
 			os.Exit(1)
 		}
@@ -68,22 +88,62 @@ func main() {
 		// Assemble the octopussified code
 		binary, err := c_octo.Assemble(octopussified)
 		if err != nil {
-			fmt.Println("\033[91;1mCould not complete assembly due to the following error:\033[0m")
-			fmt.Println(err)
+			fmt.Fprintln(os.Stderr, bad("Could not complete assembly due to the following error:"))
+			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
 
 		// And output to the destination file
-		err = os.WriteFile(output, binary, 0644)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+		if *OUTPUT_FILE == "STDOUT" {
+			fmt.Print(binary)
+		} else {
+			err = os.WriteFile(*OUTPUT_FILE, binary, 0644)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
 		}
 
 	default:
-		fmt.Printf("\033[91;1mDon't know how to convert '%s' to '%s'\033[0m\n", path.Ext(input), path.Ext(output))
+		fmt.Fprintf(os.Stderr, bad("Don't know how to convert '%s' to '%s'\n"), path.Ext(*INPUT_FILE), path.Ext(*OUTPUT_FILE))
 		os.Exit(1)
 	}
 
-	fmt.Printf("\033[92;1mFinished processing in %s\033[0m\n", time.Since(startTime))
+	fmt.Fprintf(os.Stderr, good("Finished processing in %s\n"), time.Since(startTime))
+}
+
+func good(input string) string {
+	if USING_COLOR {
+		return "\033[92;1m" + input + "\033[0m"
+	} else {
+		return input
+	}
+}
+
+func bad(input string) string {
+	if USING_COLOR {
+		return "\033[91;1m" + input + "\033[0m"
+	} else {
+		return input
+	}
+}
+
+func print_usage() {
+	fmt.Fprintln(os.Stderr, `Usage:
+   octopus -i file.8o -o result.ch8 OPTION1 OPTION2
+
+Input file should be an assembly language file in Octo syntax with the extension
+".8o". Output file can have the extensions ".8o" or ".ch8" to output either the
+Octopussified intermediate assembly language or the resulting binary. If you do
+not specify an output file, it will dump the Octopussified intermediate assembly
+to standard output.
+
+The options you provide as additional parameters will be "true" for the
+Octopussification.
+
+For more information, see:
+https://github.com/Timendus/chipcode/blob/main/octopus2/README.md
+
+Valid parameters:`)
+	flag.PrintDefaults()
 }
