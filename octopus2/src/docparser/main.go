2c402b2cce4c8edb64f7e4911cb8aaf9e4f4efc6
Timendus <mail@timendus.com>
2025-08-24 18:08:35 +0200

    Make the anchor links work on Github

--- octopus2/src/docparser/main.go
+++ octopus2/src/docparser/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	_ "embed"
 	"fmt"
+	"strings"
 	"text/template"
 )
 
@@ -12,7 +13,11 @@ var markdown string
 
 func Parse(source string, filename string, tmpl string) (string, error) {
 	data := findData(source, filename)
-	tpl, err := template.New("docparser").Parse(tmpl)
+	tpl, err := template.New("docparser").Funcs(template.FuncMap{
+		"replaceAll": strings.ReplaceAll,
+		"toLower":    strings.ToLower,
+		"toUpper":    strings.ToUpper,
+	}).Parse(tmpl)
 	if err != nil {
 		return "", fmt.Errorf("could not parse template: %v", err)
 	}
